76b1aefa40a71849c0432c1e536aa99545c6b257
nichijoux <58805009+nichijoux@users.noreply.github.com>
2021-04-22 23:58:26 +0800

    Update Client.java

--- 图书管理系统Client/src/library/Client.java
+++ 图书管理系统Client/src/library/Client.java
@@ -1,1 +1,15 @@
+package library;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+//自己的包
+import library.frame.LoginFrame;
 
+//学生管理系统类
+@SuppressWarnings("serial")
+public class Client extends JFrame {	
+	public static void main(String[] args) throws Exception {
+		//设置窗口样式
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		new LoginFrame();
+	}
+}
